e18835f16709c00b3ddd66d5afaaadb55d19db4f
Angel Marin <anmar@ndgn.net>
2016-05-29 14:13:53 +0200

    drop flip params from default config

--- src/gstsrc.c
+++ src/gstsrc.c
@@ -81,7 +81,7 @@ static gchar * stream_pipeline_video( void ) {
   gchar *pipeline = g_strdup_printf("rpicamsrc %s !"
     " video/x-h264,width=%d,height=%d,framerate=%d/1 ! "
     "h264parse ! rtph264pay config-interval=5 pt=96 name=pay0",
-      rs_args__video_args ? rs_args__video_args : "bitrate=1000000 hflip=true vflip=true",
+      rs_args__video_args ? rs_args__video_args : "bitrate=1000000",
       rs_args__video_width, rs_args__video_height, rs_args__video_frm );
   return pipeline;
 }
